03d51ca439f9aa2229677a563ab6fb670f3bbb7d
Prachiii14 <prachisinha005@gmail.com>
2026-06-30 15:31:43 +0530

    Add files via upload

---
+++ basic4.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*variable- are containerstore the data and later change it*/
+
+  /*<datatypes> <variablename> = value;*/
+  int someValue = 7;
+  
+  print(someValue);
+  
+  someValue=678;//reassigning
+  print(someValue);
+}
---
+++ basic5.dart
@@ -0,0 +1,13 @@
+void main() {
+  /*variable- are containerstore the data and later change it*/
+
+  /*<datatypes> <variablename> = value;*/
+  int someValue = 24;
+  
+  print(someValue);
+  
+  someValue=someValue - 75;//reassigning  /*how we can use operator to give value to a variable*/ 
+  someValue -=55;// short hand syntax
+  someValue =55;
+  print(someValue);
+}
