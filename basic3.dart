8cdc06d18f040dc780878f733d1d54491c1ea3ca
Prachiii14 <prachisinha005@gmail.com>
2026-06-30 13:26:22 +0530

    Add files via upload

---
+++ basic3.dart
@@ -0,0 +1,13 @@
+void main() {
+  /*variable- are containerstore the data and later change it*/
+
+  /*<datatypes> <variablename> = value;*/
+  double firstvalue = 10.5867;
+  double secondvalue = 20.55;
+
+  print(firstvalue);
+  print(secondvalue);
+  print(firstvalue + secondvalue);
+  print(firstvalue * secondvalue);
+  print(firstvalue - secondvalue);
+}
